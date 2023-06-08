e011f8f96afb9a89161e9d3442ae9eef88f775aa
tildakey <sant.marl.os.on@gmail.com>
2023-06-08 13:07:02 -0400

    changed it around

--- app/models/channel.py
+++ app/models/channel.py
@@ -54,6 +54,9 @@ class ChannelMember(db.Model):
 
     '''
     __tablename__ = 'channelmembers'
+    
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
     channel_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('channels.id'), passive_deletes=True), nullable=False)
